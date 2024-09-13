bfc10c6f909a9a05e38608e992eb45841def74b5
yhavin <y.havin@gmail.com>
2024-09-13 13:21:20 -0400

    Use realip package for rate limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"expvar"
 	"fmt"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -15,6 +14,7 @@ import (
 	"greenlight.yhavin/internal/validator"
 
 	"github.com/felixge/httpsnoop"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -60,11 +60,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
--- go.mod
+++ go.mod
@@ -11,11 +11,11 @@ require golang.org/x/time v0.6.0
 require (
 	github.com/felixge/httpsnoop v1.0.4
 	github.com/go-mail/mail/v2 v2.3.0
+	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce
 	golang.org/x/crypto v0.27.0
 )
 
 require (
-	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/mail.v2 v2.3.1 // indirect
 )
